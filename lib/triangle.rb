a2c293ba8a81cb6acc102622e411448da9445d36
Benis Tambe <will.jenifer@yahoo.com>
2021-02-13 18:05:38 -0500

    commit tree fb2dc41e7d620602e0d90c6169f3dc6281591512

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+  # write code here'
+  attr_accessor :kind
+
+  def initialize (length1, length2, length3)
+    @triangle_type = nil
+
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+
+  end
+
+  def throw_error
+    begin
+      raise TriangleError
+    rescue TriangleError => error
+      puts error.message 
+    end
+
+  end
+
+  def kind 
+
+    if @length1 < 0 || @length2 < 0 || @length3 < 0
+      throw_error
+      break;
+    elsif (@length1 + @length2) < @length3
+      throw_error
+      break;
+    else
+
+    end
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "I do not recognize that"
+    end
+  end
+
 end
